0772361ce29da11fabe6694c6af1ae32da4d8448
grada92 <danielegradassai@gmail.com>
2023-06-26 15:14:41 +0200

    modifica Tag entity

--- src/main/java/io/danielegradassai/entity/Tag.java
+++ src/main/java/io/danielegradassai/entity/Tag.java
@@ -1,10 +1,7 @@
 package io.danielegradassai.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -12,6 +9,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 @Entity
 public class Tag {
 
